2c812c7de9f87a87e49b9f1e9ea1943b9efcdc9e
Sandeep Kumar <satyasandeep786@gmail.com>
2023-12-09 20:28:20 +0530

    refactor

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -10,35 +10,32 @@ require("dotenv").config();
 // const { etherscan } = require('./hardhat.networks');
 
 module.exports = {
-    etherscan:{
-        apiKey: "T5Z532NYIYCGVB465P72DFYT96MYPHEH9T",
-    
-    },
-    solidity: {
-        version: "0.8.19",
-        settings: {
-            optimizer: {
-                enabled: true,
-                runs: 1000000,
-            },
-            viaIR: true,
-        },
-    },
-    networks: {
-        polygonmumbai: {
-            url: "https://rpc-mumbai.maticvigil.com",
-            accounts: [
-                process.env.PRIVATE_KEY,
-            ],
-        },
-    },
-    namedAccounts: {
-        deployer: {
-            default: 0,
-        },
-    },
-    gasReporter: {
-        enable: true,
-        currency: "USD",
-    },
+  etherscan: {
+    apiKey: "T5Z532NYIYCGVB465P72DFYT96MYPHEH9T"
+  },
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000000
+      },
+      viaIR: true
+    }
+  },
+  networks: {
+    polygonmumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0
+    }
+  },
+  gasReporter: {
+    enable: true,
+    currency: "USD"
+  }
 };
