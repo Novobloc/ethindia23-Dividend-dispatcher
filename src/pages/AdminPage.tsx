04440ef255ff57de826da09623848740f87439c1
Shiva Kumar Mangina <shiva2nani.mangina@gmail.com>
2023-12-10 04:30:27 +0530

    fix: Update Admin UI

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -73,19 +73,10 @@ export default function Example() {
 
   return (
     <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
-      <div className="absolute inset-x-0  -z-10 transform-gpu overflow-hidden blur-3xl" aria-hidden="true">
-        <div
-          className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
-          style={{
-            clipPath:
-              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
-          }}
-        />
-      </div>
       <div className="mx-auto max-w-2xl text-center">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact sales</h2>
-        <p className="mt-2 text-lg leading-8 text-gray-600">Aute magna irure deserunt veniam aliqua magna enim voluptate.</p>
+        <h2 className="text-3xl font-normal tracking-tight text-slate-900 sm:text-4xl">Admin</h2>
       </div>
+      <div>
       <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
         <div className="space-y-4">
           <div>
@@ -123,8 +114,8 @@ export default function Example() {
           <button
             onClick={allocateSharesFunction}
             type="submit"
-            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-            Mint
+            className="flex items-center justify-center w-full px-6 py-3 mb-3 text-lg hover:text-white text-black rounded-md sm:mb-0 bg-yellow-300 hover:bg-black">
+            Allocate Shares
           </button>
         </div>
       </form>
@@ -150,11 +141,13 @@ export default function Example() {
           <button
             type="submit"
             onClick={setDividendFunction}
-            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-            Set Dividend
+            className="flex items-center justify-center w-full px-6 py-3 mb-3 text-lg hover:text-white text-black rounded-md sm:mb-0 bg-yellow-300 hover:bg-black">
+            Publish Dividend
           </button>
         </div>
       </form>
+      </div>
+      
     </div>
   );
 }
